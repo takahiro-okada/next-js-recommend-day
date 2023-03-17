1ac089f5a5ac2f8047274ee77adeea04aa3f2643
takahiro-okada <okahanako09875@gmail.com>
2023-03-18 05:28:02 +0900

    add: Adding code

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  darkMode: false, // 'media' or 'class'
+  purge: {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    options: {
+      // https://purgecss.com/safelisting.html#patterns
+      safelist: {
+        standard: [/^bg-/, /^text-/],
+      },
+    },
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
